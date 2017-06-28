082e9a1d5267bf430c79cefa51deb02374ff30e7
Pablo E Garcia-Nieto <paedugar@gmail.com>
2017-06-28 11:49:49 -0700

    Transfer functions from fileMangement.r to misc.r

--- misc.r
+++ misc.r
@@ -4,3 +4,37 @@ printTime <- function(x = "") {
 	flush.console()
 	cat(as.character(Sys.time()), x)
 }
+
+trim <- function(x) { 
+		
+	# Trims white space from a character vector
+	
+	if(!is.character(x))
+		stop("x has to be string or a character vector")
+
+	return(gsub("\\s+", "", x))
+	
+}
+
+parseArg <- function(x, sep, trim = T) {
+		
+	#Takes a string an creates a vector using sep as the separator
+	# x - a string, if a vector only first element will be considred
+	# sep - a string, if a vector only first element will be considered
+	# trim - bool, if true white spaces will be eliminated
+		
+	if(!is.character(x))
+		stop ("x has to be a string")
+	if(!is.character(sep))
+		stop ("x has to be a string")
+	
+	x <- x[1]
+	sep <- sep[1]
+	
+	if(trim)
+		x <- trim(x)
+	
+	return (unlist(strsplit(x,sep)))
+	
+}
+
