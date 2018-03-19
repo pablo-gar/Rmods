2e3c85615f1a2600b460bd4f0877d9e59c7affad
Pablo E Garcia-Nieto <paedugar@gmail.com>
2018-03-19 13:58:16 -0700

    Change grid line intesity in theme_grid_y

--- ggthemes.R
+++ ggthemes.R
@@ -9,47 +9,58 @@ theme_base <- function(base_size = 12) {
     axis.ticks.length = unit(0.3, "lines"),
     axis.ticks.margin = unit(0.5, "lines"),
  
-    legend.background = theme_rect(colour=NA), 
-    legend.key =        theme_rect(colour = NA, ),
+    legend.background = element_rect(colour=NA), 
+    legend.key =        element_rect(colour = NA, ),
     legend.key.size =   unit(1.2, "lines"),
     legend.text =       element_text(colour = NA,size = base_size * 0.8),
     legend.title =      element_text(colour = NA,size = base_size * 0.8, face = "bold", hjust = 0),
     legend.position =   "right",
  
-    panel.background =  theme_rect(fill = NA, colour = NA), 
-    panel.border =      theme_rect(fill = NA, colour=NA), 
-    panel.grid.major =  theme_line(colour = grey90, size = 0.2),
-    panel.grid.minor =  theme_line(colour = NA, size = 0.5),
+    panel.background =  element_rect(fill = NA, colour = NA), 
+    panel.border =      element_rect(fill = NA, colour=NA), 
+    panel.grid.major =  element_line(colour = "grey90", size = 0.2),
+    panel.grid.minor =  element_line(colour = NA, size = 0.5),
     panel.margin =      unit(0.25, "lines"),
  
-    strip.background =  theme_rect(fill = NA, colour = NA), 
+    strip.background =  element_rect(fill = NA, colour = NA), 
     strip.text.x =      element_text(colour = NA,size = base_size * 0.8),
     strip.text.y =      element_text(colour = NA,size = base_size * 0.8, angle = -90),
  
-    plot.background =   theme_rect(colour = NA),
+    plot.background =   element_rect(colour = NA),
     plot.title =        element_text(colour = NA,size = base_size * 1.2),
     plot.margin =       unit(c(1, 1, 0.5, 0.5), "lines")
   ), class = "options")
 }
 
 
-
-theme_grid_y <- function (base_size = 12, base_family = "") 
+theme_noGrid <- function (base_size = 12, base_family = "") 
 {
     theme_grey(base_size = base_size, base_family = base_family) %+replace% 
         theme(
-                axis.line = element_line(colour = "black"),     
+                axis.line = element_line(colour = "grey60"),     
                 axis.text = element_text(size = rel(0.8)), 
                 axis.ticks = element_line(colour = "black"), 
                 legend.key = element_rect(colour = "grey80"), 
                 panel.background = element_rect(fill = "white", colour = NA), 
                 panel.border = element_rect(fill = NA,colour = NA),
-                panel.grid.major.y = element_line(colour = "grey80",size = 0.3),
+                panel.grid.major.y = element_blank(),
+                panel.grid.minor.y = element_blank(),
                 panel.grid.major.x = element_blank(),                
                 panel.grid.minor = element_blank(), 
                 strip.background = element_rect(fill = "white", colour = "grey50", size = 0.5)
             )
 }
+
+
+theme_grid_y <- function (base_size = 12, base_family = "") 
+{
+    theme_noGrid(base_size = base_size, base_family = base_family) %+replace% 
+        theme(
+                panel.grid.major.y = element_line(colour = "grey78",size = 0.3),
+                panel.grid.minor.y = element_line(colour = "grey88",size = 0.25),
+            )
+}
+
 
 
 theme_fullBlank <- function (base_size = 12, base_family = "") 
